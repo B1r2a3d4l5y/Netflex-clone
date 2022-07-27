782bdb6f34f97f18b8782730640f3c2c26bffc3d
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2022-07-27 10:58:40 +0200

    updated click.js in the js folder

--- js/click.js
+++ js/click.js
@@ -1,12 +1,39 @@
 $(document).ready(function(){
     $("#questionBtn1").click(function(){
-        $("#what").animate({opacity:1});
+        $("#what").animate({
+            height: "+=100px"
+        });
         $("#what").toggle();
+     
     });
-});
-$("#costBtn").click(function(){
-    $("#cost_anwser").animate({opacity:1}).toggle();
-});
-$("#watchBtn").click(function(){
-    $("#where_to_watch").animate({opacity:1}).toggle();
-})
+    $("#costBtn").click(function(){
+        $("#cost_anwser").animate({
+            height: "+=100px"
+        });
+        $("#cost_anwser").toggle();
+    });
+    $("#watchBtn").click(function(){
+        $("#where_to_watch").animate({
+            heigth: "+=100px",
+        });
+        $("#where_to_watch").toggle()
+    });
+  $("#cancelBtn").click(function(){
+    $(".cancel").animate({
+        height: "+=100px"
+    });
+    $(".cancel").toggle()
+  });
+  $(".watchBtn").click(function(){
+    $(".what_to_watch").animate({
+        height: "+=100px"
+    });
+    $(".what_to_watch").toggle()
+  });
+  $(".kidsBtn").click(function(){
+    $(".kids_content").animate({
+        height: "+=100px"
+    });
+    $(".kids_content").toggle()
+  })
+}) 
