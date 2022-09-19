643526be35a98ce8748a86c68f02abaaed037a8d
unknown <bradleyarnold918@gmail.com>
2022-09-19 13:50:05 +0200

    re-doing click.js code/fixing

--- js/click.js
+++ js/click.js
@@ -1,39 +1,6 @@
-$(document).ready(function(){
+$document.ready(function(){
     $("#questionBtn1").click(function(){
-        $("#what").animate({
-            height: "+=100px"
-        });
-        $("#what").toggle();
-     
+        $("#what").animate({opacity:1, height:"+=100px"});
     });
-    $("#costBtn").click(function(){
-        $("#cost_anwser").animate({
-            height: "+=100px"
-        });
-        $("#cost_anwser").toggle();
-    });
-    $("#watchBtn").click(function(){
-        $("#where_to_watch").animate({
-            heigth: "+=100px",
-        });
-        $("#where_to_watch").toggle()
-    });
-  $("#cancelBtn").click(function(){
-    $(".cancel").animate({
-        height: "+=100px"
-    });
-    $(".cancel").toggle()
-  });
-  $(".watchBtn").click(function(){
-    $(".what_to_watch").animate({
-        height: "+=100px"
-    });
-    $(".what_to_watch").toggle()
-  });
-  $(".kidsBtn").click(function(){
-    $(".kids_content").animate({
-        height: "+=100px"
-    });
-    $(".kids_content").toggle()
-  })
-}) 
+    $("#what").toggle()
+})
