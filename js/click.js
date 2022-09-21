97d67378b743380bca72b988052651c6fff00e5b
unknown <bradleyarnold918@gmail.com>
2022-09-21 11:03:39 +0200

    added new updates to the redo of click.js

--- js/click.js
+++ js/click.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
     $("#questionBtn1").click(function(){
-        $("#what").animate({
-            height:"+=100px",
-            opacity:1
+       
         })
-        $("#what").show(1000).toggle()
-        
+        $("#what").animate({
+            opacity: 1
+        })
+        $("#what").toggle()
     })
-})
