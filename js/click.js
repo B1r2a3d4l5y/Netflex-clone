b840cd06f14eb80f3130241846a9a793f248bd83
B1r2a3d4l5y <bradleyarnold918@gmail.com>
2022-10-10 14:19:57 +0200

    added new jquery code for the buttons

--- js/click.js
+++ js/click.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
-    $("#questionBtn1 ").click(function(){
-        // change icon  
-            $(".bi").toggleClass("bi-x bi-plus ").style = "bi-x font-size: 25px"
+    $("#questionBtn1 ").unbind("click").click(function(){
+        // change icon button1 
+        $(".bi " ).toggleClass( "bi-x , bi-plus");
        
-        $("#what").animate({opacity:1}).toggle()
+        $("#what").animate({opacity:1}).toggle();
+    });
+    $("#questionBtn2").click(function(){
+        // toggle icons button2
+        $(".bi ").toggleClass("bi-x , bi-plus ")
+        $(".cost").animate({opacity:1}).toggle()
     })
 })
